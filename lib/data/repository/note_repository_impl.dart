70c84784e612ae4c9476a27175c950a26e862d5c
minhaneul <onlynforever@gmail.com>
2022-09-11 12:52:43 +0900

    Add: noteRepository 작성, sqflite 적용

---
+++ lib/data/repository/note_repository_impl.dart
@@ -0,0 +1,34 @@
+import '../../domain/model/note.dart';
+import '../../domain/repository/note_repository.dart';
+import '../data_source/note_db_helper.dart';
+
+class NoteRepositoryImpl implements NoteRepository {
+  final NoteDbHelper db;
+
+  NoteRepositoryImpl(this.db);
+
+  @override
+  Future<List<Note>> getNotes() async{
+    return await db.getNotes();
+  }
+
+  @override
+  Future<Note?> getNoteById(int id) async{
+    return await db.getNoteById(id);
+  }
+
+  @override
+  Future<void> insertNote(Note note) async{
+    await db.insertNote(note);
+  }
+
+  @override
+  Future<void> updateNote(Note note) async {
+    await db.updateNote(note);
+  }
+
+  @override
+  Future<void> deleteNote(Note note) async {
+    await db.deleteNote(note);
+  }
+}
